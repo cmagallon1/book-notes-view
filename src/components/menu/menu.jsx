53414d62976f557354318826abbca6b273b99a6d
cmagallon1 <cesar9mv@gmail.com>
2020-03-09 17:42:29 -0600

    [add] validations

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
 import NavigationBar from '../navbar/navbar';
+import Cookies from 'js-cookie';
+import axios from 'axios';
 
 class Menu extends Component {
+
+  state = {
+    books: []
+  }
+
+  takeError = (error) => {
+    if(error.response) 
+      return error.response
+    else if(error.request)
+      return error.request
+    else
+      return error.message
+  }
+
+  myHeader = () => {
+    return {
+      headers: {
+        'Authorization': Cookies.get('token'),
+        'Content-Type': 'application/json'
+      }
+    }
+  }
+
+  componentDidMount(){
+    axios.get('https://book-notes-api.herokuapp.com/books', this.myHeader)
+    .then(res =>{ console.log(res)})
+    .catch(err => console.log(this.takeError(err)))
+  }
+
   render(){
-    return(
-      <>
-        <NavigationBar/>
-        <h1>menu</h1>
-      </>
-    );
+    if(Cookies.get('token')) {
+      return(
+        <>
+          <NavigationBar/>
+          <h1>menu</h1>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <NavigationBar/>
+          <h1>You're not logged</h1>
+        </>
+      )
+    }
   }
 }
 
--- src/components/routes/routes.jsx
+++ src/components/routes/routes.jsx
@@ -3,11 +3,13 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect
 } from 'react-router-dom';
 import Signin from '../signin/signin';
 import Signup from '../signup/signup';
 import Menu from '../menu/menu';
 import NoMatch from '../signin/signin';
+import Cookies from 'js-cookie';
 
 const Routes = () => {
   return (
@@ -25,12 +27,20 @@ const Routes = () => {
         <Route exact path="/dashboard">
           <Menu/>
         </Route>
+        <Route exact path="/signout">
+          <Signout/>          
+        </Route>
         <Route path="*">
           <NoMatch/>
         </Route>
       </Switch>
     </Router>
   )
+}
+
+const Signout = () => {
+  Cookies.remove('token')
+  return ( <Redirect to={{ pathname: "/" }}/>)
 }
 
 export default Routes
